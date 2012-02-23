6d04c1f5caa97d9b37f696a216233f5b0d4cbab5
Rylan Hazelton <rylan@badger.com>
2012-02-23 12:37:53 -0800

    revert revert fix

--- javascripts/lib/hasher-0.1.0/elements.js
+++ javascripts/lib/hasher-0.1.0/elements.js
@@ -98,8 +98,8 @@ with (Hasher()) {
         var serialized_form = {};
         var elems = this.getElementsByTagName('*');
         for (var i=0; i < elems.length; i++) {
-          if (elems[i].name) {
-            if ((elems[i].tagName != 'FORM') && elems[i].name) {
+          if ((elems[i].tagName != 'FORM') && elems[i].name) {
+            if (elems[i].tagName == 'SELECT') {
               // TODO: support multiple select
               serialized_form[elems[i].name] = elems[i].options[elems[i].selectedIndex].value;
             } else if ((['radio', 'checkbox'].indexOf(elems[i].getAttribute('type')) == -1) || elems[i].checked) {
