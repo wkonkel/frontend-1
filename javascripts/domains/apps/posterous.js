5e530113e9ad31268a6d3230f70b587c8499c445
Eastagile <thanhthao_thtt@yahoo.com>
2012-02-27 16:10:30 +0700

    [f] App Improvements: Posterous Spaces [https://www.pivotaltracker.com/story/show/25494711]

--- javascripts/domains/apps/posterous.js
+++ javascripts/domains/apps/posterous.js
@@ -28,9 +28,16 @@ with (Hasher('Posterous', 'DomainApps')) {
       h1({ 'class': 'header-with-right-btn' }, div({ 'class': 'long-domain-name' }, 'POSTEROUS FOR ' + domain)),
       domain_app_settings_button('badger_posterous', domain),
 
-      div("Posterous DNS settings have been installed into Badger DNS.",
-          'Also check out ',
-          a({ href: 'http://posterous.uservoice.com/knowledgebase/articles/36303-setting-up-posterous-spaces-with-a-third-party-reg', target: '_blank' }, 'Posterous Custom Domains'), '.')
+      p("Posterous DNS settings have successfully been installed into Badger DNS."),
+      div(
+        span("Last steps before you're all set:"), br(),
+        span("1. Log in to ", a({ href: "http://posterous.com/", target: '_blank' }, "Posterous"), "."), br(),
+        span("2. Click on ", strong("Manage Spaces"), "."), br(),
+        span("3. Under the appropriate Space, click on the gear icon, then click on ", strong ("Space Settings"), "."), br(),
+        span("4. Click ", strong("Edit"), " beside your Space."), br(),
+        span("5.  Under ", strong("Setup a custom domain I already own"), ", enter your domain, " + domain + ", then click on ", strong("Save Settings"), "."), br()
+      ),
+      p (span("For more information, ", a({ href: 'http://posterous.uservoice.com/knowledgebase/articles/36303-setting-up-posterous-spaces-with-a-third-party-reg', target: '_blank' }, 'click here'), "."))
     );
   });
 
