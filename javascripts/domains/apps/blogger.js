6221a199ce9d39d5eddd29de522cc24aed4793bb
Eastagile <thanhthao_thtt@yahoo.com>
2012-02-27 16:09:19 +0700

    [f] App Improvements: Blogger [https://www.pivotaltracker.com/story/show/25481533]

--- javascripts/domains/apps/blogger.js
+++ javascripts/domains/apps/blogger.js
@@ -32,9 +32,17 @@ with (Hasher('Blogger', 'DomainApps')) {
       h1({ 'class': 'header-with-right-btn' }, div({ 'class': 'long-domain-name' }, 'BLOGGER FOR ' + domain)),
       domain_app_settings_button('badger_blogger', domain),
 
-      div("Blogger DNS settings have been installed into Badger DNS.",
-          'Also check out ',
-          a({ href: 'http://support.google.com/blogger/bin/static.py?hl=en&ts=1233381&page=ts.cs', target: '_blank' }, 'Blogger Custom Domains'), '.')
+      p("Blogger DNS settings have successfully been installed into Badger DNS."),
+      div(
+        span("Last steps before you're all set:"), br(),
+        span("1. Log in to ", a({ href: "http://www.blogger.com", target: '_blank' }, "Blogger"), "."), br(),
+        span("2. Go to the ", strong("Settings|Basic"), " tab."), br(),
+        span("3. Under ", strong("Publishing"), ", click ", strong("+ Add a custom domain"), "."), br(),
+        span("4. Click on ", strong("Switch to advanced settings"), "."), br(),
+        span("5. Enter your domain, " + domain + ", into the box provided and click ", strong("Save"), "."), br()
+      ),
+      p("Once everything is set up and saved, your Blogger URL, -enter Blogger URL-, will automatically redirect to " + domain + "."),
+      p (span("For more information, ", a({ href: 'http://support.google.com/blogger/bin/static.py?hl=en&ts=1233381&page=ts.cs', target: '_blank' }, 'click here'), "."))
     );
   });
 
