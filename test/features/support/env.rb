57c2036ab3b618686be5048a11c9d79e9e29c247
Warren Konkel <wkonkel@gmail.com>
2012-02-28 13:54:50 -0800

    headless

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -2,6 +2,17 @@ require 'capybara'
 require 'capybara/dsl'
 require 'capybara/cucumber'
 
+if ENV['HEADLESS'] == 'true'
+  require 'headless'
+
+  headless = Headless.new
+  headless.start
+
+  at_exit do
+    headless.destroy
+  end
+end
+
 Capybara.default_driver = :selenium
 Capybara.default_wait_time = 0.5
 
@@ -10,3 +21,4 @@ Capybara.register_driver :selenium do |app|
 end
 Capybara.app_host = 'file://' + File.expand_path(File.dirname(__FILE__) + '/../../..')
 World(Capybara)
+
