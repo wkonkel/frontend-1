a41d43e8e6b2940b5f2d57a545f4f63d0c803267
Paul Makepeace <Paul.Makepeace@realprogrammers.com>
2012-04-16 14:47:38 -0700

    Use Chrome in preference to Firefox, if available

--- test/features/support/env.rb
+++ test/features/support/env.rb
@@ -6,6 +6,10 @@ Capybara.default_driver = :selenium
 Capybara.default_wait_time = 1.0
 Capybara.app_host = 'file://' + File.expand_path(File.dirname(__FILE__) + '/../../..')
 
+def command?(command)
+  system("which #{command} > /dev/null 2>&1")
+end
+
 Capybara.register_driver :selenium do |app|
   if ENV['HEADLESS'] == 'true'
     require 'headless'
@@ -14,9 +18,15 @@ Capybara.register_driver :selenium do |app|
     at_exit { headless.destroy }
 
     Capybara::Selenium::Driver # this line ensures that the top level Selenium module is loaded
-    profile = Selenium::WebDriver::Firefox::Profile.new
+    if command?('google-chrome')
+      browser, profile = :chrome, Selenium::WebDriver::Chrome::Profile.new
+    elsif command?('firefox')
+      browser, profile = :firefox, Selenium::WebDriver::Firefox::Profile.new
+    else
+      fail "Neither firefox nor google-chrome found in #{ENV['PATH']}"
+    end
     profile['general.useragent.override'] = 'Selenium'
-    Capybara::Selenium::Driver.new(app, :browser => :firefox, :profile => profile)
+    Capybara::Selenium::Driver.new(app, :browser => browser, :profile => profile)
   else
     Capybara::Selenium::Driver.new(app, :browser => :chrome, :switches => ['--user-agent=Selenium'])
   end
