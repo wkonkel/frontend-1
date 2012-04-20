b45e34cb8b29e7a836a43ddf5b940e601dc81711
Warren Konkel <wkonkel@gmail.com>
2012-04-19 17:37:46 -0700

    coming soon

--- javascripts/help/welcome.js
+++ javascripts/help/welcome.js
@@ -9,13 +9,14 @@ with (Hasher('Welcome','Application')) {
         ),
         img({ src: 'images/badger-6.png', 'class': 'badger', style: 'margin-top: 50px' }),
 
-        h3({ style: "margin-bottom: 0" }, 'Automatic transfers with no downtime.'),
+        h3({ style: "margin-bottom: 0" }, 'Automatic transfers with no downtime:'),
         div({ style: 'height: 120px' },
           app_icon('images/apps/godaddy.png', 'GoDaddy'),
           app_icon('images/apps/ns.png', 'Network Solutions'),
-          app_icon('images/apps/enom.png', 'eNom'),
-          app_icon('images/apps/namecheap.png', 'Namecheap'),
-          app_icon('images/apps/1and1.png', '1&1')
+          img({ src: 'images/v2/comingsoon.png', style: 'padding: 9px' })
+          // app_icon('images/apps/enom.png', 'eNom'),
+          // app_icon('images/apps/namecheap.png', 'Namecheap'),
+          // app_icon('images/apps/1and1.png', '1&1')
         ),
         //h3({ style: "margin-bottom: 0" }, 'Free WHOIS privacy and DNS hosting.'),
         h3('Shortcuts for easy setup:'),
