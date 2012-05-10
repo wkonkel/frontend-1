502b19a7085c5fcbfd0a2a34a96a1633d6dd6b58
Cory Boyd <corytheboyd@gmail.com>
2012-05-09 19:02:27 -0700

    account settings

--- javascripts/accounts/account.js
+++ javascripts/accounts/account.js
@@ -1,6 +1,6 @@
 with (Hasher('Account','Application')) {
   route('#account', function() {
-    var account_setings = div();
+    var account_setings = div(spinner('Loading...'));
 
     render(
       h1('My Account » Settings'),
@@ -10,13 +10,9 @@ with (Hasher('Account','Application')) {
     );
 
     BadgerCache.getAccountInfo(function(response) {
-      render({ into: account_setings },
-        ul(
-          li( a({ href: curry(show_modal, Account.change_password_modal()) }, "Change Password") ),
-          li( a({ href: curry(show_modal, Account.change_name_modal(response.data)) }, "Change First/Last Name") ),
-          li( a({ href: curry(show_modal, Account.change_email_modal(response.data)) }, "Change Email Address") )
-        )
-      )
+      render({ into: account_setings }, 
+        change_account_form(response.data)
+      );
     });
   });
   
@@ -48,91 +44,50 @@ with (Hasher('Account','Application')) {
     ));
   });
   
-	define('change_password', function(data) {
-		if(data.new_password != data.confirm_password)
-			return $('#change-password-messages').empty().append( error_message({ data: { message: "Passwords do not match" } }) );
-		
-		Badger.changePassword(data, function(response) {
-			if (response.meta.status == 'ok') {
-				Badger.logout();
-				$('#change-password-messages').empty().append( success_message(response) );
-				$('#change-password-form').empty();
-			} else {
-				$('#change-password-messages').empty().append( error_message(response) );	
-			}
-		});
-	});
+  define('change_account_form', function(account_info) {
+    if (account_info.name) {
+      var first_name = account_info.name.split(' ')[0];
+      var last_name = account_info.name.split(' ')[1];
+    }
+    
+    return form_with_loader({ 'class': 'fancy', action: submit_account_change_form, loading_message: 'Submitting changes...' },
+      h3('Change Account Details'),
+      
+      div({ id: 'messages' }),
 
-	define('change_name', function(form_data) {
-		if(form_data.first_name == '' || form_data.last_name == '')
-			return $('#change-name-messages').empty().append( error_message({ data: { message: "First name and last name cannot be blank" } }) );
-
-		Badger.changeName(form_data, function(response) {
-			if (response.meta.status == 'ok') {
-        Application.update_account_name();
-        set_route(get_route());
-				$('#change-name-messages').empty().append( success_message(response) );
-        $('#change-name-form').empty();
-			} else {
-				$('#change-name-messages').empty().append( error_message(response) );
-			}
-		});
-	});
-
-	define('change_email', function(form_data) {
-		if(form_data.email == '')
-			return $('#change-email-messages').empty().append( error_message({ data: { message: "Email cannot be blank" } }) );
-
-		Badger.changeEmail(form_data, function(response) {
-			if (response.meta.status == 'ok') {
-        Badger.logout();
-				$('#change-email-messages').empty().append( success_message(response) );
-				$('#change-email-form').empty();
-			} else {
-				$('#change-email-messages').empty().append( error_message(response) );
-			}
-		});
-	});
-
-	define('change_password_modal', function(data) {
-		data = data || {};
-		return form({ action: change_password },
-			h1("CHANGE PASSWORD"),
-			div({ id: 'change-password-messages' }),
-			div({ id: 'change-password-form', style: 'text-align: center; margin: 20px 0' },
-				input({ name: 'new_password', type: 'password', 'class': 'fancy', placeholder: 'New Password', value: data.new_password || '' }),
-				input({ name: 'confirm_password', type: 'password', 'class': 'fancy', placeholder: 'Confirm Password', value: data.confirm_password || '' }),
-				input({ 'class': 'myButton', type: 'submit', value: 'Update' })
-			)
-		);
-	});
-
-	define('change_name_modal', function(data) {
-    data = (data || {});
-    var first_name = (data.name || '').split(' ')[0];
-    var last_name = (data.name || '').replace(first_name,"");
-    return form({ action: change_name },
-      h1("CHANGE FIRST/LAST NAME"),
-      div({ id: 'change-name-messages' }),
-      div({ id: 'change-name-form', style: 'text-align: center; margin: 20px 0' },
-        input({ name: 'first_name', 'class': 'fancy', placeholder: 'First Name', value: first_name || '' }),
-        input({ name: 'last_name', 'class': 'fancy', placeholder: 'Last Name', value: last_name  || '' }),
-        input({ 'class': 'myButton', type: 'submit', value: 'Update' })
+      fieldset(
+        label({ 'for': 'first_name' }, 'First and last name'),
+        text({ 'class': 'short right-margin', name: 'first_name', placeholder: first_name || 'John' }),
+        text({ 'class': 'short', name: 'last_name', placeholder: last_name || 'Smith' })
+      ),
+      
+      fieldset(
+        label({ 'for': 'password' }, 'Change password'),
+        password({ name: 'password', placeholder: 'abc123' })
+      ),
+      
+      fieldset(
+        label({ 'for': 'email' }, 'New email'),
+        text({ name: 'email', placeholder: account_info.email || 'john@badger.com' })
+        // text({ 'class': 'short', name: 'confirm_email', placeholder: 'john@badger.com' })
+      ),
+      
+      fieldset({ 'class': 'no-label' },
+        submit({ id: 'submit-changes-button', value: 'Save' })
       )
-    );
-
-	});
-
-	define('change_email_modal', function(data) {
-    data = (data || {});
-    return form({ action: change_email },
-      h1("CHANGE EMAIL ADDRESS"),
-      div({ id: 'change-email-messages' }),
-      div({ id: 'change-email-form', style: 'text-align: center; margin: 20px 0' },
-        input({ name: 'email', 'class': 'fancy', placeholder: 'New Email Address', value: data.email || '' }),
-        input({ 'class': 'myButton', type: 'submit', value: 'Update' })
-      )
-    );
-	});
+    )
+  });
+  
+  define('submit_account_change_form', function(form_data) {
+    Badger.updateAccount(form_data, function(response) {
+      if (response.meta.status == 'ok') {
+        $('#messages').html(success_message('Your account has been updated.'));
+      } else {
+        $('#messages').html(error_message(response));
+      }
+      
+      hide_form_submit_loader()
+    });
+  });
 
 }
