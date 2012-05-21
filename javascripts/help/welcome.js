d951e2078b80224a537e4d092258cd09d3603541
Cory Boyd <corytheboyd@gmail.com>
2012-05-21 16:13:18 -0700

    home page tweaks: badger image scales correctly in IE. header background color fixed for IE

--- javascripts/help/welcome.js
+++ javascripts/help/welcome.js
@@ -7,7 +7,7 @@ with (Hasher('Welcome','Application')) {
       div({ id: 'main' },
 
         div({ 'id': 'content-top' }, 
-          img({ src: 'images/badger-6.png', style: 'float: right; width: 150px; margin: 20px 50px 20px 20px' }),
+          img({ src: 'images/badger-6.png', style: 'float: right; width: 150px; height: auto; margin: 20px 50px 20px 20px' }),
           h2("Domain management done ", span({ style: 'letter-spacing: 2px' }, "r"), "ight."),
           //h3("Badger makes managing your domains easy, wherever they are."),
 
