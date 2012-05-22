1e7fb529a76239f9468fbce4ede40fdb93dc5cfc
Cory Boyd <corytheboyd@gmail.com>
2012-05-22 13:44:12 -0700

    fix Object.keys in IE

--- javascripts/lib/ie_fix.js
+++ javascripts/lib/ie_fix.js
@@ -161,3 +161,15 @@ var OutlineFix = {
     }
   }
 }
+
+/**********************************************************************************
+ * Fix Object.keys for IE/Opera
+ **********************************************************************************/
+ 
+if(!Object.keys) Object.keys = function(o){
+  if (o !== Object(o))
+    throw new TypeError('Object.keys called on non-object');
+  var ret=[], p;
+  for(p in o) if (Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
+  return ret;
+}
