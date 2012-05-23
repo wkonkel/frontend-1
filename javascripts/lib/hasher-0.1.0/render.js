a384926d7388060f93ace15a38f5e8a19165827e
Warren Konkel <wkonkel@gmail.com>
2012-05-23 14:29:58 -0700

    rework layout method to be slightly less hacky... perhaps this was done for a reason?

--- javascripts/lib/hasher-0.1.0/render.js
+++ javascripts/lib/hasher-0.1.0/render.js
@@ -22,20 +22,16 @@ with (Hasher()) {
   });
   
   define('layout', function(name, callback) {
-    define(name, function callback_wrapper(yield) {
-      // NOTE: this approach might be sketchy... we're storing state in the function object itself
-      if (!callback_wrapper.layout_elem) {
+    var layout_elem, yield_parent;
+    define(name, function() {
+      if (!layout_elem) {
         var tmp_div = div();
-        callback_wrapper.layout_elem = callback(tmp_div);
-        callback_wrapper.yield_parent = tmp_div.parentNode;
+        layout_elem = callback(tmp_div);
+        yield_parent = tmp_div.parentNode;
       }
       
-      // replace contents and add content
-      callback_wrapper.yield_parent.innerHTML='';
-      yield = flatten_to_array(yield);
-      for (var i=0; i < yield.length; i++) callback_wrapper.yield_parent.appendChild((typeof(yield[i]) == 'string') ? document.createTextNode(yield[i]) : yield[i]);
-      
-      return callback_wrapper.layout_elem;
+      render({ into: yield_parent, layout: false }, arguments);
+      return layout_elem;
     });
   });
 
