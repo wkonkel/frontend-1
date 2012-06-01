834b3fe8dd0a0b685909caeb4d6abb924bed382e
Cory Boyd <corytheboyd@gmail.com>
2012-06-01 12:31:55 -0700

    long poll condition for unknown registrar

--- javascripts/domains/show.js
+++ javascripts/domains/show.js
@@ -25,7 +25,10 @@ with (Hasher('DomainShow','DomainApps')) {
       action: {
         method: curry(Badger.getDomain, domain),
         on_ok: function(response) {
-          if (response.data && (response.data.available || !response.data.current_registrar.match(/unknown/i))) {
+          
+          console.log(response);
+          
+          if (response.data && (response.data.available || !response.data.current_registrar.match(/^unknown$/i))) {
             handle_get_domain_response(content_div, domain, response);
             return true;
           }
