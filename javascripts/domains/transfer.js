9a2a186678183fc6b641e216ff177d62ed3e2389
Camille Vergara <camille@badger.com>
2012-06-07 14:35:27 -0700

    "transfer for _ credits" instead of "register" on transfer screen

--- javascripts/domains/transfer.js
+++ javascripts/domains/transfer.js
@@ -126,7 +126,7 @@ with (Hasher('Transfer','Application')) {
           ),
           
           fieldset({ 'class': 'no-label' },
-            submit({ name: 'submit', value: 'Register for ' + domain_count + ' Credit' + (domain_count != 1 ? 's' : '') })
+            submit({ name: 'submit', value: 'Transfer for ' + domain_count + ' Credit' + (domain_count != 1 ? 's' : '') })
           )
         )
       );
