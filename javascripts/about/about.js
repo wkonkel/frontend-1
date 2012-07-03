4ed138253d6e2256b3e2a23fc682daff27483c90
Cory Boyd <corytheboyd@gmail.com>
2012-07-02 17:26:17 -0700

    wording change. more badger.

--- javascripts/about/about.js
+++ javascripts/about/about.js
@@ -71,8 +71,8 @@ with (Hasher('About', 'Application')) {
       div({ id: 'logo' }, img({ src: 'images/v2/happybadger.png' })),
       h2("Domain management you'll enjoy."),
       p("Seriously, we make domains so easy that a first grader could do 'em!*"), 
-      p("Domains effectively drive the entire internet, shouldn't they be easier to manage? We thought so, and thus, Badger was born! You shouldn't have to auction off your house and sacrifice your first born son to transfer domains, you should be able to press a button that says \"Transfer Domain\" and be done with it. That is our philosophy, and we think you will appreciate it."),
-      p("Stop letting domain registrars badger you! Err, you can let us badger you, but it will be a pleasant badgering!"),
+      p("Domains effectively drive the entire internet, shouldn't they be easier to manage? We thought so, and thus, Badger was born! You shouldn't have to auction off your house and sacrifice your first born to transfer domains, you should be able to press a button that says \"Transfer Domain\" and be done with it. That is our philosophy, and we think you will appreciate it."),
+      p("Stop letting domain registrars badger you, and start using... Badger!"),
       span({ style: 'font-size: 10px; text-align: right' }, '*Must be 18 or older. Sorry kids.')
     );
   });
