f22af0f657a99d2873231c238e305bbff77bc71f
Cory Boyd <corytheboyd@gmail.com>
2012-07-12 15:45:44 -0700

    show rewards

--- javascripts/accounts/rewards.js
+++ javascripts/accounts/rewards.js
@@ -25,15 +25,52 @@ with (Hasher('Rewards','Application')) {
           url_base = 'https://www.badger.com/';
           
       if (referral_codes.length > 0) {
-        var referral_code_obj = referral_codes[0];
+        var referral_code = referral_codes[0],
+            rewards = response.data.rewards || [],
+            referral_stats = response.data.referral_stats || {},
+            rewards_div = div();
+        
         render({ into: referral_code_div },
           info_message({ style: 'text-align: center; border: none; background: #F1F1F1' },
             h2({ style: 'margin: 0px' }, 'My Referral Code'),
-            input({ 'class': 'fancy', style: 'font-size: 20px; text-align: center; width: 450px; color: #707070; cursor: pointer', readonly: true, value: (url_base + referral_code_obj.slug), onClick: function(e) { e.target.select() } })
+            input({ 'class': 'fancy', style: 'font-size: 20px; text-align: center; width: 450px; color: #707070; cursor: pointer', readonly: true, value: (url_base + referral_code), onClick: function(e) { e.target.select() } })
           ),
-          
-          div({ id: 'more_content_goes_here' })
-        )
+
+          div({ id: 'referral-status', style: 'margin: 20px auto;' },
+            table({ style: 'width: 100%' }, tbody(
+              tr(
+                td({ style: 'text-align: center' },
+                  p({ 'class': 'rewards-points' }, (referral_stats.points_earned||0)+''),
+                  span({ style: 'margin: auto;' }, 'Points Earned')
+                ),
+                td({ style: 'width: 80%' },
+                  div({ 'class': 'meter small green nostripes', style: 'margin: 15px; height: 20px;' },
+                    span({ style: 'height: 20px; width: ' + (referral_stats.points_earned||0)+'' + '%' })
+                  )
+                )
+              )
+            ))
+          ),
+
+          rewards_div
+        );
+        
+        render({ into: rewards_div },
+          table({ 'class': 'fancy-table' }, tbody(
+            tr({ 'class': 'table-header' },
+              th({ style: 'width: 8%; text-align: center' }, 'Points'),
+              th({ style: 'width: 75%' }, 'Action'),
+              th('Completed On')
+            ),
+            rewards.map(function(reward) {
+              return tr(
+                td({ style: 'text-align: center; font-weight: bold' }, reward.point_value),
+                td(reward.description),
+                td(date(reward.created_at).toString('MMMM dd yyyy'))
+              );
+            })
+          ))
+        );
       } else {
         var default_slug = response.data.name.replace(/\s+/,'').toLowerCase();
         
