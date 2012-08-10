12292cd064d95fa6ee654c379f0d00528ebfab90
Cory Boyd <corytheboyd@gmail.com>
2012-08-09 17:03:23 -0700

    hide messages for unsupported tlds

--- javascripts/domains/show.js
+++ javascripts/domains/show.js
@@ -42,7 +42,7 @@ with (Hasher('DomainShow','DomainApps')) {
         
         return;
       } else if (domain_obj.available) {
-        if (domain_obj.can_register) {
+        if (domain_obj.can_register && domain_obj.supported_tld) {
           render({ into: target_div },
             success_message({ style: 'text-align: center' },
               h3("This domain is available!"),
@@ -81,7 +81,7 @@ with (Hasher('DomainShow','DomainApps')) {
           (domain_obj.permissions_for_person||[]).includes('pending_transfer') && display_transfer_status(domain_obj),
           
           // if this domain is not a badger registration, bluntly advertise that it can be transfered!
-          !(domain_obj.permissions_for_person||[]).includes('modify_dns') && !(domain_obj.permissions_for_person||[]).includes('pending_transfer') && info_message({ style: 'text-align: center' },
+          domain_obj.supported_tld && !(domain_obj.permissions_for_person||[]).includes('modify_dns') && !(domain_obj.permissions_for_person||[]).includes('pending_transfer') && info_message({ style: 'text-align: center' },
             p({ style: 'margin: 0px;' }, 'Do you own this domain, and want to transfer it to us? If so, add it to your cart!'),
             a({ 'class': 'myButton', style: 'margin-top: 10px;', href: function() { Cart.add_domain(domain_obj); set_route('#cart'); } }, 'Add ' + Domains.truncate_domain_name(domain) + ' to Cart')
           ),
